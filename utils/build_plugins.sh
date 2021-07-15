fb707cdcd18263dcf059aa2561ee13cc14fd9a80
JCash <mwesterdahl76@gmail.com>
2021-07-15 16:21:13 +0200

    Added pre compiled editor jar plugin

---
+++ utils/build_plugins.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Run from the project foler (containing the game.project)
+
+set -e
+
+PROJECT=defold-rive
+
+BOB=~/work/defold/tmp/dynamo_home/share/java/bob.jar
+SERVER=https://build.defold.com
+SERVER=http://localhost:9000
+VARIANT=debug
+
+TARGET_DIR=./$PROJECT/plugins
+mkdir -p $TARGET_DIR
+
+
+
+function copyfile() {
+    local path=$1
+    local folder=$2
+    if [ -f "$path" ]; then
+        if [ ! -d "$folder" ]; then
+            mkdir -v -p $folder
+        fi
+        cp -v $path $folder
+    fi
+}
+
+function copy_results() {
+    local platform=$1
+    local platform_ne=$2
+
+    # Copy the .jar files
+    for path in ./build/$platform_ne/$PROJECT/*.jar; do
+        copyfile $path $TARGET_DIR/lib/common
+    done
+
+    # Copy the files to the target folder
+    for path in ./build/$platform_ne/*.dylib; do
+        copyfile $path $TARGET_DIR/lib/$PLATFORM
+    done
+}
+
+
+function build_plugin() {
+    local platform=$1
+    local platform_ne=$2
+
+    java -jar $BOB --platform=$platform build --build-plugins --variant $VARIANT --build-server=$SERVER
+
+    copy_results $platform $platform_ne
+}
+
+
+build_plugin "x86_64-darwin" "x86_64-osx"
+build_plugin "x86_64-linux"  "x86_64-linux"
+build_plugin "x86_64-win32"  "x86_64-win32"
+
+tree $TARGET_DIR
