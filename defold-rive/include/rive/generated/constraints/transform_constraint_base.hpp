2dee2815ff8564d2b28edcb39bc14c745d2e6443
JCash <mwesterdahl76@gmail.com>
2021-08-22 10:54:25 +0200

    Updated headers
    Sha1: adfd61a779bbef3dcf5406a9c0cb6bbd1952c2d8

---
+++ defold-rive/include/rive/generated/constraints/transform_constraint_base.hpp
@@ -0,0 +1,39 @@
+#ifndef _RIVE_TRANSFORM_CONSTRAINT_BASE_HPP_
+#define _RIVE_TRANSFORM_CONSTRAINT_BASE_HPP_
+#include "rive/constraints/transform_space_constraint.hpp"
+namespace rive
+{
+	class TransformConstraintBase : public TransformSpaceConstraint
+	{
+	protected:
+		typedef TransformSpaceConstraint Super;
+
+	public:
+		static const uint16_t typeKey = 83;
+
+		/// Helper to quickly determine if a core object extends another without
+		/// RTTI at runtime.
+		bool isTypeOf(uint16_t typeKey) const override
+		{
+			switch (typeKey)
+			{
+				case TransformConstraintBase::typeKey:
+				case TransformSpaceConstraintBase::typeKey:
+				case TargetedConstraintBase::typeKey:
+				case ConstraintBase::typeKey:
+				case ComponentBase::typeKey:
+					return true;
+				default:
+					return false;
+			}
+		}
+
+		uint16_t coreType() const override { return typeKey; }
+
+		Core* clone() const override;
+
+	protected:
+	};
+} // namespace rive
+
+#endif
---
+++ defold-rive/include/rive/math/aabb.hpp
@@ -0,0 +1,47 @@
+#ifndef _RIVE_AABB_HPP_
+#define _RIVE_AABB_HPP_
+
+#include "rive/math/mat2d.hpp"
+#include "rive/math/vec2d.hpp"
+#include <cstddef>
+
+namespace rive
+{
+	class AABB
+	{
+	public:
+		union
+		{
+			float buffer[4];
+			struct
+			{
+				float minX, minY, maxX, maxY;
+			};
+		};
+
+	public:
+		AABB();
+		AABB(const AABB& copy);
+		AABB(float minX, float minY, float maxX, float maxY);
+
+		inline const float* values() const { return buffer; }
+
+		float& operator[](std::size_t idx) { return buffer[idx]; }
+		const float& operator[](std::size_t idx) const { return buffer[idx]; }
+
+		static void center(Vec2D& out, const AABB& a);
+		static void size(Vec2D& out, const AABB& a);
+		static void extents(Vec2D& out, const AABB& a);
+		static void combine(AABB& out, const AABB& a, const AABB& b);
+		static bool contains(const AABB& a, const AABB& b);
+		static bool isValid(const AABB& a);
+		static bool testOverlap(const AABB& a, const AABB& b);
+		static bool areIdentical(const AABB& a, const AABB& b);
+		static void transform(AABB& out, const AABB& a, const Mat2D& matrix);
+
+		float width() const;
+		float height() const;
+		float perimeter() const;
+	};
+} // namespace rive
+#endif
