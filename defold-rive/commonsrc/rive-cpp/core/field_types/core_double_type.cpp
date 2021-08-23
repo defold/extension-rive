35a945e399e0d46f501bd6420e3f27e9f2fa594e
JCash <mwesterdahl76@gmail.com>
2021-08-23 09:51:34 +0200

    Removed rive-cpp source

--- defold-rive/commonsrc/rive-cpp/core/field_types/core_double_type.cpp
+++
@@ -1,9 +0,0 @@
-#include "core/field_types/core_double_type.hpp"
-#include "core/binary_reader.hpp"
-
-using namespace rive;
-
-double CoreDoubleType::deserialize(BinaryReader& reader) 
-{
-    return reader.readFloat32();
-}
--- defold-rive/commonsrc/rive-cpp/node.cpp
+++
@@ -1,6 +0,0 @@
-#include "node.hpp"
-
-using namespace rive;
-
-void Node::xChanged() { markTransformDirty(); }
-void Node::yChanged() { markTransformDirty(); }
