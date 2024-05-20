3309b2dadb177ba2ff6d3721b9e18d239570a48d
Jhonny Göransson <jhonny.goransson@gmail.com>
2024-05-20 15:49:39 +0200

    Updage libs and remove those that doesn't work

--- defold-rive/include/renderer.h
+++ defold-rive/include/renderer.h
@@ -41,6 +41,8 @@ namespace dmRive
     rive::Factory*               GetRiveFactory(HRenderContext context);
     rive::Renderer*              GetRiveRenderer(HRenderContext context);
     rive::Mat2D                  GetViewTransform(HRenderContext context, dmRender::HRenderContext render_context);
+    rive::Mat2D                  GetViewProjectionTransform(HRenderContext context, dmRender::HRenderContext render_context);
+    rive::Mat2D                  GetViewProjTransform(HRenderContext context, dmRender::HRenderContext render_context);
     void                         GetDimensions(HRenderContext context, uint32_t* width, uint32_t* height);
     void                         RenderBegin(HRenderContext context, ShaderResources* shaders, dmResource::HFactory factory);
     void                         RenderEnd(HRenderContext context);
