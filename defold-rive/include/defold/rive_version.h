f64733325f2b4905ac9a3eaef25c3cb06c3b0196
JCash <mwesterdahl76@gmail.com>
2025-09-19 12:31:16 +0200

    updated headers

--- defold-rive/include/defold/rive_version.h
+++ defold-rive/include/defold/rive_version.h
@@ -1,9 +1,8 @@
 // Generated. Do not edit! See ./utils/write_rive_version_header.py
 
-//Add profiler Tag macro (#10593) 859d6283c9
-//Update profiler_macros.h
+//fix(webgpu): Fix Y orientation on GL zero and nonzero FBOs (#10623) 454b0d2db1
 //
-//Co-authored-by: John White <aliasbinman@gmail.com>
-const char* RIVE_RUNTIME_AUTHOR="aliasbinman";
-const char* RIVE_RUNTIME_DATE="2025-09-16T17:31:47Z";
-const char* RIVE_RUNTIME_SHA1="5591fac1b3d767c170df6c7738b038909a5c5e39";
+//Co-authored-by: Chris Dalton <99840794+csmartdalton@users.noreply.github.com>
+const char* RIVE_RUNTIME_AUTHOR="csmartdalton";
+const char* RIVE_RUNTIME_DATE="2025-09-19T02:18:27Z";
+const char* RIVE_RUNTIME_SHA1="e54883d9099f87ed6d87c678793fd619b5594e2c";
