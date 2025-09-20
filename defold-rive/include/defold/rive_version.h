e2404a292699fa03855d1ef33bf18eb53d2cd836
JCash <mwesterdahl76@gmail.com>
2025-09-20 15:54:51 +0200

    Rebuilt android libraries using ndk=r25 and using rive-runtime repo build scripts

--- defold-rive/include/defold/rive_version.h
+++ defold-rive/include/defold/rive_version.h
@@ -1,8 +1,9 @@
-// Generated. Do not edit! See ./utils/write_rive_version_header.py
+// Generated. Do not edit! See ./build_version_header.sh
 
 //fix(webgpu): Fix Y orientation on GL zero and nonzero FBOs (#10623) 454b0d2db1
 //
 //Co-authored-by: Chris Dalton <99840794+csmartdalton@users.noreply.github.com>
+
 const char* RIVE_RUNTIME_AUTHOR="csmartdalton";
-const char* RIVE_RUNTIME_DATE="2025-09-19T02:18:27Z";
+const char* RIVE_RUNTIME_DATE="2025-09-19T02:18:27+00:00";
 const char* RIVE_RUNTIME_SHA1="e54883d9099f87ed6d87c678793fd619b5594e2c";
