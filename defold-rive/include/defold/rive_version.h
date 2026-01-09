6bb7cf0a65ea6f9800464ea411c0ccfa8600b6c0
Mathias Westerdahl <mwesterdahl76@gmail.com>
2026-01-09 12:42:13 +0100

    initial update to push the branch

--- defold-rive/include/defold/rive_version.h
+++ defold-rive/include/defold/rive_version.h
@@ -1,14 +1,11 @@
 // Generated. Do not edit! See ./build_version_header.sh
 
-//fix(wgpu): Bit rot (#11245) 47f4ec46d4
-//* Clean up some assumptions in WebGPU that support for PLS meant it was
-//  actually being used. Now that we support MSAA, it might not be in use.
+//Nnnnn fix memory lua issues (#11412) ba94f03ad0
+//* push scripted drawable to stack twice to ensure it is not destroyed before use
+//* code improvements
 //
-//* Clean up a refactoring typo that caused an input attachment to be
-//  deleted before it was used.
-//
-//Co-authored-by: Chris Dalton <99840794+csmartdalton@users.noreply.github.com>
+//Co-authored-by: hernan <hernan@rive.app>
 
-const char* RIVE_RUNTIME_AUTHOR="csmartdalton";
-const char* RIVE_RUNTIME_DATE="2025-12-09T23:23:00Z";
-const char* RIVE_RUNTIME_SHA1="3b5d9785f579e8055ec869382caceee19958c3ed";
+const char* RIVE_RUNTIME_AUTHOR="bodymovin";
+const char* RIVE_RUNTIME_DATE="2026-01-09T07:54:52+00:00";
+const char* RIVE_RUNTIME_SHA1="3a5b5058aca009b823154ed7f92c11d599c00ae3";
